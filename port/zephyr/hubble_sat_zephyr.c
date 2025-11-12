HubbleNetwork/sdk

commit e735a2b5fc66024029f010e0bd53e92db60427a3

port: split system and sat functionality up
Break out the sat and system functionality from the single port header
to constituant parts. This will make it simpler for applications not
utilizing satellite functionality to implement only the necessary
API functionality.

Signed-off-by: Paul Buckley <paul@hubble.com>

port/zephyr/hubble_sat_zephyr.c
@@ -10,7 +10,7 @@
 #include <zephyr/kernel.h>
 
 #include <hubble/sat.h>
-#include <hubble/hubble_port.h>
+#include <hubble/port/sys.h>
 #include <hubble_rf_zephyr.h>
 
 #define HUBBLE_WAIT_SYMBOL_US          8000U

include/hubble/port/sat_radio.h
@@ -1,0 +1,103 @@
+/*
+ * Copyright (c) 2024 Hubble Network, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef INCLUDE_HUBBLE_PORT_SAT_RADIO_H
+#define INCLUDE_HUBBLE_PORT_SAT_RADIO_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <hubble/sat/packet.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Hubble Satellite Network porting API.
+ * @defgroup hubble_sat_port Hubble Satellite Network porting APIs
+ *
+ * Functions and data structures that are used to port the Hubble Satellite
+ * Network for different platforms.
+ *
+ * @{
+ */
+struct hubble_sat_api {
+	/**
+	 * @brief Transmits a packet.
+	 *
+	 * @param packet A pointer to a hubble_packet structure containing the
+	 *               data to be transmitted.
+	 *
+	 * @return 0 on success, non-zero on error.
+	 */
+	int (*transmit_packet)(const struct hubble_sat_packet *packet);
+
+	/**
+	 * @brief Sets the transmission power
+	 *
+	 * Set the power that will be used when transmitting data.
+	 *
+	 * @param power The desired power level as an 8-bit signed integer
+	 *              (~int8_t~). The valid range of values depends on the
+	 *              specific implementation.
+	 *
+	 * @return 0 on success, non-zero on error.
+	 *
+	 */
+	int (*power_set)(int8_t power);
+
+	/**
+	 * @brief Sets the channel
+	 *
+	 * Set the channel that will be used when transmitting data.
+	 *
+	 * @param channel The desired communication channel.
+	 *                The valid range of values depends on the specific implementation.
+	 *
+	 * @return 0 on success, non-zero on error.
+	 */
+	int (*channel_set)(uint8_t channel);
+
+	/**
+	 * @brief Enables Hubble Network satellite stack.
+	 *
+	 * Setup everything that is need to transmit data.
+	 *
+	 * @return 0 on success, non-zero on error.
+	 */
+	int (*enable)(void);
+
+	/**
+	 * @brief Disables Hubble Network satellite stack.
+	 *
+	 * This API disables the satellite stack and restore devices to the
+	 * state they were before.
+	 *
+	 * @return 0 on success, non-zero on error.
+	 */
+	int (*disable)(void);
+};
+
+/**
+ * @brief Retrieves the satellite API interface.
+ *
+ * This function provides access to the satellite API, which includes logging
+ * and packet transmission functionalities. The API is typically implemented
+ * as a singleton.
+ *
+ * @return A pointer to a constant struct hubble_sat_api.
+ *         Returns NULL if the API is unavailable.
+ */
+const struct hubble_sat_api *hubble_sat_api_get(void);
+
+/** @} */ /* hubble_sat_port */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INCLUDE_HUBBLE_PORT_SAT_RADIO_H */

include/hubble/port/sys.h
@@ -4,13 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef INCLUDE_HUBBLE_HUBBLE_PORT_H
-#define INCLUDE_HUBBLE_HUBBLE_PORT_H
+#ifndef INCLUDE_HUBBLE_PORT_SYS_H
+#define INCLUDE_HUBBLE_PORT_SYS_H
 
 #include <stddef.h>
 #include <stdint.h>
-
-#include <hubble/sat/packet.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -39,6 +37,7 @@ enum hubble_log_level {
 #define HUBBLE_LOG_INFO(...)    HUBBLE_LOG(HUBBLE_LOG_INFO, __VA_ARGS__)
 #define HUBBLE_LOG_WARNING(...) HUBBLE_LOG(HUBBLE_LOG_WARNING, __VA_ARGS__)
 #define HUBBLE_LOG_ERR(...)     HUBBLE_LOG(HUBBLE_LOG_ERR, __VA_ARGS__)
+
 /**
  * @brief Function pointer to retrieve the target system uptime.
  *
@@ -89,88 +88,8 @@ uint16_t hubble_sequence_counter_get(void);
  */
 int hubble_log(enum hubble_log_level level, const char *format, ...);
 
-/**
- * @brief Hubble Satellite Network porting API.
- * @defgroup hubble_sat_port Hubble Satellite Network porting APIs
- *
- * Functions and data structures that are used to port the Hubble Satellite
- * Network for different platforms.
- *
- * @{
- */
-struct hubble_sat_api {
-	/**
-	 * @brief Transmits a packet.
-	 *
-	 * @param packet A pointer to a hubble_packet structure containing the
-	 *               data to be transmitted.
-	 *
-	 * @return 0 on success, non-zero on error.
-	 */
-	int (*transmit_packet)(const struct hubble_sat_packet *packet);
-
-	/**
-	 * @brief Sets the transmission power
-	 *
-	 * Set the power that will be used when transmitting data.
-	 *
-	 * @param power The desired power level as an 8-bit signed integer
-	 *              (~int8_t~). The valid range of values depends on the
-	 *              specific implementation.
-	 *
-	 * @return 0 on success, non-zero on error.
-	 *
-	 */
-	int (*power_set)(int8_t power);
-
-	/**
-	 * @brief Sets the channel
-	 *
-	 * Set the channel that will be used when transmitting data.
-	 *
-	 * @param channel The desired communication channel.
-	 *                The valid range of values depends on the specific implementation.
-	 *
-	 * @return 0 on success, non-zero on error.
-	 */
-	int (*channel_set)(uint8_t channel);
-
-	/**
-	 * @brief Enables Hubble Network satellite stack.
-	 *
-	 * Setup everything that is need to transmit data.
-	 *
-	 * @return 0 on success, non-zero on error.
-	 */
-	int (*enable)(void);
-
-	/**
-	 * @brief Disables Hubble Network satellite stack.
-	 *
-	 * This API disables the satellite stack and restore devices to the
-	 * state they were before.
-	 *
-	 * @return 0 on success, non-zero on error.
-	 */
-	int (*disable)(void);
-};
-
-/**
- * @brief Retrieves the satellite API interface.
- *
- * This function provides access to the satellite API, which includes logging
- * and packet transmission functionalities. The API is typically implemented
- * as a singleton.
- *
- * @return A pointer to a constant struct hubble_sat_api.
- *         Returns NULL if the API is unavailable.
- */
-const struct hubble_sat_api *hubble_sat_api_get(void);
-
-/** @} */ /* hubble_sat_port */
-
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* INCLUDE_HUBBLE_HUBBLE_PORT_H */
+#endif /* INCLUDE_HUBBLE_PORT_SYS_H */

port/freertos/hubble_freertos.c
@@ -7,7 +7,7 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-#include <hubble/hubble_port.h>
+#include <hubble/port/sys.h>
 
 /**
  * Define weak attribute.

port/zephyr/hubble_zephyr.c
@@ -12,7 +12,7 @@
 #include <errno.h>
 #include <stddef.h>
 
-#include <hubble/hubble_port.h>
+#include <hubble/port/sys.h>
 
 LOG_MODULE_REGISTER(hubblenetwork, CONFIG_HUBBLE_LOG_LEVEL);
 

samples/freertos/ti/ble-beacon/src/hubble_ble_ti.c
@@ -13,7 +13,7 @@
 #include <ti/drivers/cryptoutils/cryptokey/CryptoKeyPlaintext.h>
 
 #include <hubble/ble.h>
-#include <hubble/hubble_port.h>
+#include <hubble/port/sys.h>
 #include <hubble/port/crypto.h>
 
 #define BLE_ADV_LEN 31

samples/zephyr/ble-beacon/src/store.c
@@ -9,7 +9,7 @@
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/fs/nvs.h>
 
-#include <hubble/hubble_port.h>
+#include <hubble/port/sys.h>
 
 #define NVS_PARTITION		storage_partition
 #define NVS_PARTITION_DEVICE	FIXED_PARTITION_DEVICE(NVS_PARTITION)

src/crypto/mbedtls.c
@@ -6,7 +6,7 @@
 #include <mbedtls/cmac.h>
 
 #include <hubble/ble.h>
-#include <hubble/hubble_port.h>
+#include <hubble/port/sys.h>
 #include <hubble/port/crypto.h>
 
 /* Older verions of Zephyr do not define it */

commit ac6121d84c8000f0b1c463eacce880127c45a2ad

sat: Fix a problem with missing header
Fix an issue introduced in e735a2b5fc66024029f010e0bd53e92db60427a3.

Signed-off-by: Flavio Ceolin <flavio@hubblenetwork.com>

port/zephyr/hubble_sat_zephyr.c
@@ -10,6 +10,7 @@
 #include <zephyr/kernel.h>
 
 #include <hubble/sat.h>
+#include <hubble/port/sat_radio.h>
 #include <hubble/port/sys.h>
 #include <hubble_rf_zephyr.h>
 