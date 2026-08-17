fa206bca9ed6368309ab18f31b5929094dcaf2d8
Flavio Ceolin <flavio.ceolin@gmail.com>
2026-08-17 15:32:13 -0700

    zephyr: nrf: Use mpsl fem with softdevice
    Use mpsl fem when building using nrf-sdk + softdevice controller.
    
    Signed-off-by: Flavio Ceolin <flavio@hubble.com>

--- port/zephyr/boards/nordic/nrf52/fem.h
+++ port/zephyr/boards/nordic/nrf52/fem.h
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef CONFIG_MPSL_FEM_API_AVAILABLE
+
 void hubble_board_fem_setup(void);
 
 void hubble_board_fem_enable(void);
@@ -11,3 +13,23 @@ void hubble_board_fem_enable(void);
 void hubble_board_fem_bypass(void);
 
 void hubble_board_fem_sleep(void);
+
+#else
+
+static inline void hubble_board_fem_setup(void)
+{
+}
+
+static inline void hubble_board_fem_enable(void)
+{
+}
+
+static inline void hubble_board_fem_bypass(void)
+{
+}
+
+static inline void hubble_board_fem_sleep(void)
+{
+}
+
+#endif /* !CONFIG_MPSL_FEM_API_AVAILABLE */
